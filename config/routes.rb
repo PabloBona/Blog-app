522c46a58da3f44990c5a8c2ce4aa268551ef9f7
PabloBona <pablobonasera@gmail.com>
2023-09-24 13:48:23 -0300

    Update views Update routes to match with requirements

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # resources :users, only: [:new, :create]
-  # Defines the root path route ("/")
-  # Def root "users#index"
   root 'users#index'
-  # Def users :id route http://127.0.0.1:3000/users/1
+
+  get '/users', to: 'users#index', as: 'users'
   get '/users/:id', to: 'users#show', as: 'user'
-  # Def users :id/posts route
   get '/users/:id/posts', to: 'users#posts', as: 'user_posts'
-  # Def users :id/posts/post:id  route
   get '/users/:id/posts/:post_id', to: 'posts#show', as: 'user_post'
+  get '/posts', to: 'posts#index', as: 'posts'  # Agregamos esta ruta
 
 end
