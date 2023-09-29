8c389a1137080f1b453c0f8a95cbd6056c900196
PabloBona <pablobonasera@gmail.com>
2023-09-29 01:40:10 -0300

    Fix lint

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,12 +1,12 @@
-require "test_helper"
+require 'test_helper'
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
+  test 'should get new' do
     get comments_new_url
     assert_response :success
   end
 
-  test "should get create" do
+  test 'should get create' do
     get comments_create_url
     assert_response :success
   end
