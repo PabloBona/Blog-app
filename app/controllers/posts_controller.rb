0e376ac260c3f9cea35272731a0d165e26237120
PabloBona <pablobonasera@gmail.com>
2023-09-30 16:24:43 -0300

    Add eager loading Update styles

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts.page(params[:page]).per(10)
+    @posts = @user.posts.includes(:author, :comments, :likes).page(params[:page]).per(10)
   end
 
   def show
