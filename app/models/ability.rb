24a180f4cb86c5a3080f4d6c2b3dd79332d5bb31
PabloBona <pablobonasera@gmail.com>
2023-10-05 19:22:52 -0300

    Update ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,11 +6,11 @@ class Ability
       can :manage, :all
     else
       can :read, :all
-      can :create, Comment, author_id: user.id
+      can :create, Comment, user_id: user.id
       can :create, Post, author_id: user.id
       can :create, Like
       can :destroy, Comment do |item|
-        item.author_id == user.id
+        item.user_id == user.id
       end
       can :destroy, Post do |item|
         item.author_id == user.id
